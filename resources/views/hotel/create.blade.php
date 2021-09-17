770272f5b6f6ebcab6560bb0355b0786c4e73b78
e277 <ezramuir12@gmail.com>
2021-09-17 18:42:20 -0500

    Update hotel create, navbar blade

--- resources/views/hotel/create.blade.php
+++ resources/views/hotel/create.blade.php
@@ -5,10 +5,21 @@
 <main>
     <div>
         <h1>Enter Hotel Name</h1>
+        <div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div><br />
+        </div>
+    @endif
         <form action="{{ route('hotel.store') }}" method="post">
             @csrf
             <div>
-                <input type="text" name="hotel_name">
+                <input type="text" name="hotel_name" placeholder="Hotel name...">
             </div>
             <div>
                 <input type="submit" value="Submit">
