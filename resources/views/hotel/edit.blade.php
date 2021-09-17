a875ac27268dc543e764297ef8986d41370872d7
e277 <ezramuir12@gmail.com>
2021-09-16 20:03:39 -0500

    first modification of hotel folder

---
+++ resources/views/hotel/edit.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Edit Hotel</h1>
+        <form action="hotel" method="post">
+            @csrf
+            <div>
+                <input type="hidden" name="hotel_id"  >
+            </div>
+            <div>
+                <input type="text" name="hotel_name">
+            </div>
+            <div>
+                <input type="submit" value="Update">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
